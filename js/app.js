09afa949444bf6d94301a8903e19d95413752bcc
Deon Choi <deon.choi@gmail.com>
2019-11-07 21:48:12 -0800

    Added Keypress capability
    User is able to use keyboard to press keys on screen, similar to a real piano. The 'C' key will play the 'C' note, etc.

--- js/app.js
+++ js/app.js
@@ -14,4 +14,66 @@ window.onload = () => {
         notes[i].addEventListener('click', play);
     }
 
+    document.addEventListener('keydown', e => {
+        let audio;
+        switch (e.keyCode) {
+            case 65:
+                audio = document.getElementById('a').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 66:
+                audio = document.getElementById('b').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 67:
+                audio = document.getElementById('c').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 68:
+                audio = document.getElementById('d').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 69:
+                audio = document.getElementById('e').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 70:
+                audio = document.getElementById('f').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+            case 71:
+                audio = document.getElementById('g').childNodes[1];
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.currentTime = 0
+                }
+                break;
+        }
+    } );
+
 }
